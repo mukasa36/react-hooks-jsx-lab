0e40d2b50d364a7cc9b7fd52d52700c8b602fd81
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-07-21 12:15:14 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const user ={name,city}
+console.log(name,city)
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={{color:"firebrick"}}>
+      Liza is a Web Developer from New York
+    </h1>
+  </div>
+  )
 }
 
 export default Home;
